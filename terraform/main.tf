3703fa4bc82ffec4f4099f2b173c607c9098359f
Denys Skvortsov <48939274+skvortsovden@users.noreply.github.com>
2025-05-22 16:58:10 +0200

    Add terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,5 +14,33 @@ provider "google" {
 resource "google_project" "website_project" {
   name       = var.project_name
   project_id = var.project_name
+  billing_account = var.billing_account_id
 }
 
+resource "google_storage_bucket" "website_bucket" {
+  name     = var.bucket_name
+  location = var.location
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_service_account" "website_service_account" {
+  account_id   = "website-service-account"
+  display_name = "Website Service Account"
+  project      = google_project.website_project.project_id
+}
+
+resource "google_storage_bucket_iam_member" "website_bucket_iam_member" {
+  bucket = google_storage_bucket.website_bucket.name
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.website_service_account.email}"
+}
+
+# Make bucket public by granting allUsers storage.objectViewer access
+resource "google_storage_bucket_iam_member" "public_rule" {
+  bucket = google_storage_bucket.website_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -11,3 +11,22 @@ variable "project_name" {
     type        = string
     default     = "skvortsovden-website-project"
 }
+
+variable "billing_account_id" {
+    description = "Billing account ID"
+    type        = string
+    default     = "01B636-A34826-F48A6B"
+}
+
+variable "location" {
+    description = "The GCP location to deploy resources in"
+    type        = string
+    default     = "US"
+}
+
+variable "bucket_name" {
+    description = "Name of the GCP storage bucket"
+    type        = string
+    default     = "skvortsovden-website-bucket"
+  
+}
